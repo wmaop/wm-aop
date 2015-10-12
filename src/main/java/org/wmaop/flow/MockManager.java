3da2622ad7d76aa71726b432b1f628f7a13efa56
richard <richard@sysgen.co.uk>
2015-10-12 19:56:05 +0100

    Add exception handling

--- src/main/java/org/wmaop/flow/MockManager.java
+++ src/main/java/org/wmaop/flow/MockManager.java
@@ -1,17 +1,15 @@
 package org.wmaop.flow;
 
-import java.io.IOException;
-import java.util.Collections;
+import java.lang.reflect.InvocationTargetException;
 
 import org.apache.log4j.Logger;
 import org.wmaop.aop.chainprocessor.AOPChainProcessor;
 import org.wmaop.aop.chainprocessor.Interceptor;
 import org.wmaop.interceptor.assertion.Assertion;
 import org.wmaop.interceptor.assertion.AssertionInterceptor;
-import org.wmaop.interceptor.assertion.AssertionManager;
 import org.wmaop.interceptor.mock.canned.CannedResponseInterceptor;
+import org.wmaop.interceptor.mock.exception.ExceptionInterceptor;
 
-import com.softwareag.g11n.util.CollectionUtil;
 import com.wm.app.b2b.server.ServiceException;
 import com.wm.data.IData;
 import com.wm.data.IDataCursor;
@@ -19,12 +17,13 @@ import com.wm.data.IDataUtil;
 
 public class MockManager extends AbstractFlowManager {
 
-	static final String ADVICE_ID = "adviceId";
-	static final String RESPONSE = "response";
-	static final String INTERCEPT_POINT = "interceptPoint";
-	static final String SERVICE_NAME = "serviceName";
-	static final String CONDITION = "condition";
-
+	public static final String ADVICE_ID = "adviceId";
+	public static final String RESPONSE = "response";
+	public static final String INTERCEPT_POINT = "interceptPoint";
+	public static final String SERVICE_NAME = "serviceName";
+	public static final String CONDITION = "condition";
+	public static final String EXCEPTION = "exception";
+			
 	
 	private static final Logger logger = Logger.getLogger(MockManager.class);
 	
@@ -78,5 +77,25 @@ public class MockManager extends AbstractFlowManager {
 		pipelineCursor = pipeline.getCursor();
 		IDataUtil.put(pipelineCursor, "invokeCount", invokeCount);
 		pipelineCursor.destroy();
+	}
+	
+	public void registerException(IData pipeline) throws ServiceException {
+		
+		IDataCursor pipelineCursor = pipeline.getCursor();
+		String adviceId = IDataUtil.getString(pipelineCursor, ADVICE_ID);
+		String interceptPoint = IDataUtil.getString(pipelineCursor, INTERCEPT_POINT);
+		String serviceName = IDataUtil.getString(pipelineCursor, SERVICE_NAME);
+		String pipelineCondition = IDataUtil.getString(pipelineCursor, CONDITION);
+		String exception = IDataUtil.getString(pipelineCursor, EXCEPTION);
+		pipelineCursor.destroy();
+
+		mandatory(pipeline, "{0} must exist when creating an assertion", ADVICE_ID, INTERCEPT_POINT, SERVICE_NAME, EXCEPTION);
+		
+		try {
+			Exception e = (Exception) Class.forName(exception).getDeclaredConstructor(String.class).newInstance("WMAOP " + serviceName);
+			registerInterceptor(adviceId, interceptPoint, serviceName, pipelineCondition, new ExceptionInterceptor(e));
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+			throw new ServiceException(e);
+		}
 	}
 }
