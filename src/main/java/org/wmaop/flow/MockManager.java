b0ee49895e7f4f038a29122b58a5a65a96a1db1d
Richard <richard@sysgen.co.uk>
2016-05-25 17:47:33 +0100

    Tidy imports

--- src/main/java/org/wmaop/flow/MockManager.java
+++ src/main/java/org/wmaop/flow/MockManager.java
@@ -1,9 +1,7 @@
 package org.wmaop.flow;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
--- src/main/java/org/wmaop/interceptor/pipline/PipelineCaptureInterceptor.java
+++ src/main/java/org/wmaop/interceptor/pipline/PipelineCaptureInterceptor.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.wmaop.aop.interceptor.FlowPosition;
